9226af9eda4bbd217af173f43dc10297c08fc2de
ijse <i@ijser.cn>
2015-04-20 16:32:13 +0800

    always output startup infomation

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ var server = app.listen(3000, 'localhost', function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  log('Listening at http://%s:%s', host, port);
+  console.log('Listening at http://%s:%s', host, port);
 
 });
 
