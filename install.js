d459d56043f176f3a78c72f2d2bfbdc62e7f52a2
ijse <i@ijser.cn>
2015-05-10 23:26:48 +0800

    add plugin installer

--- install.js
+++ install.js
@@ -1,20 +1,3 @@
 #!/usr/bin/env node
 
-var config = require('./configs');
-var exec = require('child_process').exec;
-
-var resolve = require('path').resolve;
-var pluginDir = config.pluginDir;
-
-var pluginName = process.argv[2];
-
-var fullName = 'datahub-plugin-' + pluginName;
-
-exec('npm install ' + fullName, function(e, so, se) {
-  if(e) throw e;
-
-  if(pluginDir) {
-    exec('mv -v ./node_modules/' + fullName + ' ' + resolve(pluginDir + '/' + pluginName));
-  }
-});
-
+require('./lib/pluginInstaller').install(process.argv[2]);
